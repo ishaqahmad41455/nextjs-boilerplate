4d689e771f051dbb1528933b8b28b5d86bc3b7de
Ishaq Ahmad Khan <103864675+ishaqahmad41455@users.noreply.github.com>
2025-05-23 13:36:23 -0700

    fixes

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'standalone'
+// };
+
+// export default nextConfig;
