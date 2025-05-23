08d8cf93e6bffb530e6a2a866426328c57a1d265
Ishaq Ahmad Khan <103864675+ishaqahmad41455@users.noreply.github.com>
2025-05-23 11:59:03 -0700

    hgf

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'standalone'
+// };
+
+// export default nextConfig;
+
+module.exports = {
+  output: 'standalone',
+};
